5295babf51865e8aa3138969f2321991346fbf31
PrithviRana1 <pzr5169@psu.edu>
2023-03-06 18:52:53 -0500

    Fixed issues via flake8

--- calculation.py
+++ calculation.py
@@ -1,13 +1,15 @@
 import repo_config
-
-
-
-#On occasion api returns severities as moderate or low instead of medium and minor
-#Added those to the dict with the score of the severity that they seem most closely associated to
-severity_dict = {'critical': 1, 'high': 0.8,'moderate': 0.5, 'medium': 0.5, 'minor': 0.1, 'low' : 0.1}
+# On occasion api returns severities
+# as moderate or low instead of medium and minor
+# Added those to the dict with the score
+# of the severity that they seem most closely associated to
+severity_dict = {'critical': 1, 'high': 0.8, 'moderate': 0.5,
+                 'medium': 0.5, 'minor': 0.1, 'low': 0.1}
 
 severity_score = [severity_dict[x] for x in repo_config.severities]
 
-health_status = repo_config.num_of_dependencies/(repo_config.num_of_vulnerabilities*(sum(severity_score))) if repo_config.num_of_vulnerabilities > 0 else 0
+health_status = (repo_config.num_of_dependencies /
+                 (repo_config.num_of_vulnerabilities*(sum(severity_score)))
+                 if repo_config.num_of_vulnerabilities > 0 else 0)
 
 print(health_status)
--- repo_config.py
+++ repo_config.py
@@ -10,33 +10,21 @@ owner = input("Enter repo owner name: ")
 repo = input("Enter repo name: ")
 base = input("Enter name of base branch: ")
 head = input("Enter name of head branch: ")
-
-
-
-
-url = 'https://api.github.com/repos/{}/{}/dependency-graph/compare/{}...{}'.format(owner, repo, base, head)
-headers = {'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28', 
-           'Authorization': 'Bearer ' + access_token_file['token'] }
+url = 'https://api.github.com/repos/{}/{}/dependency-graph/compare/{}...{}'.format(owner, repo, base, head) # noqa
+headers = {'Accept': 'application/vnd.github+json',
+           'X-GitHub-Api-Version': '2022-11-28',
+           'Authorization': 'Bearer ' + access_token_file['token']}
 response = requests.get(url, headers=headers)
-
-
-
 dependency_graph_diff = json.loads(response.text)
 num_of_dependencies = len(dependency_graph_diff)
 
 try:
-    severities = [v["severity"] for dict in dependency_graph_diff 
-                for v in dict["vulnerabilities"]]
-    num_of_vulnerabilities = len(severities)  
-except:
+    severities = [v["severity"] for dict in dependency_graph_diff
+                  for v in dict["vulnerabilities"]]
+    num_of_vulnerabilities = len(severities)
+except (AttributeError, TypeError):
     print(json.loads(response.text))
-
-
-
-#print(json.loads(response.text))
-#print(severities)
-#print(num_of_vulnerabilities)
-#print(num_of_dependencies)
-
-
-
+# print(json.loads(response.text))
+# print(severities)
+# print(num_of_vulnerabilities)
+# print(num_of_dependencies)
