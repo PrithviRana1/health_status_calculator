287e7e528f25372285c1d363324b66b15a6a5264
PrithviRana1 <pzr5169@psu.edu>
2023-03-08 15:17:21 -0500

    Added classes, seperated Github connector from token and header generation logic

--- calculation.py
+++ calculation.py
@@ -1,15 +1,29 @@
 import repo_config
-# On occasion api returns severities
-# as moderate or low instead of medium and minor
-# Added those to the dict with the score
-# of the severity that they seem most closely associated to
-severity_dict = {'critical': 1, 'high': 0.8, 'moderate': 0.5,
-                 'medium': 0.5, 'minor': 0.1, 'low': 0.1}
 
-severity_score = [severity_dict[x] for x in repo_config.severities]
 
-health_status = (repo_config.num_of_dependencies /
-                 (repo_config.num_of_vulnerabilities*(sum(severity_score)))
-                 if repo_config.num_of_vulnerabilities > 0 else 0)
+class calculation:
+    variables = repo_config.ForumulaParams()
+    variables.collect()
+    # On occasion api returns severities
+    # as moderate or low instead of medium and minor
+    # Added those to the dict with the score
+    # of the severity that they seem most closely associated to
 
-print(health_status)
+    def get_severity_score(self):
+        severity_dict = {'critical': 1, 'high': 0.8, 'moderate': 0.5,
+                         'medium': 0.5, 'minor': 0.1, 'low': 0.1}
+        self.severity_score = [severity_dict[x] for x in
+                               self.variables.severities]
+
+    def get_health_status(self):
+        self.get_severity_score()
+        health_status = (self.variables.num_of_dependencies /
+                         (self.variables.num_of_vulnerabilities *
+                          (sum(self.severity_score)))
+                         if self.variables.num_of_vulnerabilities > 0 else 0)
+
+        return health_status
+
+
+test = calculation()
+print(test.get_health_status())
