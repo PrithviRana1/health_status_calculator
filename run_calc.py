53ae76638adf2d6813be0e8829eddbdb0fdf65d5
PrithviRana1 <prithvirana022@gmail.com>
2023-03-21 16:43:03 -0400

    Fixed flake8 issues in run_calc

--- run_calc.py
+++ run_calc.py
@@ -2,21 +2,18 @@ from core import calculation
 import logging
 
 test = calculation.calc()
- 
+
 # Create and configure logger
 logging.basicConfig(filename="result.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
- 
+
 # Creating an object
 logger = logging.getLogger()
- 
+
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.DEBUG)
- 
+
 # Logging Result
 for status in test.all_statuses():
     logger.debug(status)
-
-
-
