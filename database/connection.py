ca7400cf2dbc0f01f3a56cd335b32dd4e464038b
PrithviRana1 <prithvirana022@gmail.com>
2023-04-06 09:48:44 -0400

    Fixed small errors

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,10 @@
 import psycopg2
 
-
 class Connect:
     conn = psycopg2.connect(
         database="healthdb",
         user="postgres",
         password="password",
-        host="health_status_calc-database-1",
+        host="health_status_calculator-database-1",
         port="5432"
     )
